028f3e565edc4d6914a8b58e2ecc9e77e154eb10
Daniel Márquez <daniel.marquez@ciclosmontecastelo.com>
2023-10-19 20:44:15 +0200

    Agregar archivos de proyecto.

---
+++ WebMontecastelo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebMontecastelo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
