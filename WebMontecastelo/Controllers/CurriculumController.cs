82049437020d6aeec0d03b3a6b45f317505b4855
Daniel Márquez <daniel.marquez@ciclosmontecastelo.com>
2023-10-20 15:56:24 +0200

    corrección merge

--- WebMontecastelo/Controllers/CurriculumController.cs
+++ WebMontecastelo/Controllers/CurriculumController.cs
@@ -4,12 +4,9 @@ namespace WebMontecastelo.Controllers
 {
     public class CurriculumController : Controller
     {
-<<<<<<< HEAD
-        public IActionResult Index()
-=======
+
         [HttpGet]
         public ActionResult MiCurriculum()
->>>>>>> f67c0bd (Agregar archivos de proyecto.)
         {
             return View();
         }
