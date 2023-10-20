e15e6a024af108f62340b21d4736aa2a1e5031a5
Daniel Márquez <daniel.marquez@ciclosmontecastelo.com>
2023-10-20 15:30:02 +0200

    Correción del botón a cv

--- WebMontecastelo/Controllers/CurriculumController.cs
+++ WebMontecastelo/Controllers/CurriculumController.cs
@@ -1,9 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-<<<<<<< HEAD
-=======
-using System.Diagnostics;
-using WebMontecastelo.Models;
->>>>>>> f67c0bd (Agregar archivos de proyecto.)
 
 namespace WebMontecastelo.Controllers
 {
